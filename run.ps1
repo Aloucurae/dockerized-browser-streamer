ed9766f7c960fb28510dc98babccc371a2fa87e2
Alex jonas <alexj.desantanna@gmail.com>
2022-04-18 06:23:50 -0300

    added git ignore

--- run.ps1
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --env "MEETING_URL=https://www.youtube.com/embed/GtTCebBghkQ?vq=hd1080&autoplay=1" --env "RTMP_URL=rtmp://sao01.contribute.live-video.net/app/live_41606168_8cUlEsj3DqxdAj0srSneCo8genclBb" -v $PWD/:/home/user/app -p 5900:5900  webrtc-streamer
