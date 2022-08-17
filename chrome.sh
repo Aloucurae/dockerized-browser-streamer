534d911cdaa6deec3bf54e4111294a54b2e0057f
Alex.js <alexj.desantanna@gmail.com>
2022-08-16 23:13:42 -0300

    fix chrome streaming audio delay

--- chrome.sh
+++ chrome.sh
@@ -23,7 +23,9 @@ mkdir -p /tmp/chrome-profile
 # Start Chrome browser and point it at the URL we want to capture
 exec google-chrome \
   --window-size=${SCREEN_WIDTH},${SCREEN_HEIGHT} \
+  --autoplay-policy=no-user-gesture-required \
   --no-sandbox \
+  --disable-gpu \
   --disable-sync \
   --no-first-run \
   --user-data-dir=/tmp/chrome-profile \
